3788468c8e8a4d8a350c9e78f914877228f35bf2
Pedro Rangel Junior <pedro.curtis.rangel@gmail.com>
2021-04-08 10:05:24 -0300

    Finalizando o CRUD de lançamentos

---
+++ src/main/java/br/com/repository/IDaoLancamento.java
@@ -0,0 +1,14 @@
+package br.com.repository;
+
+import java.util.List;
+
+import br.com.entidades.Lancamento;
+
+public interface IDaoLancamento {
+
+	
+	List<Lancamento> consultar(Long codUser);
+
+	
+	
+}
